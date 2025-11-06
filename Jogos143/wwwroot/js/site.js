8de1e3f650e348bbc0fffae794787381938a8b2e
Diogo <diogo.ozeliero@aluno.senai.br>
2025-11-06 18:34:18 -0300

    java da barra de pesquisa

--- Jogos143/wwwroot/js/site.js
+++ Jogos143/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const searchInput = document.getElementById('search');
 
-// Write your JavaScript code.
+searchInput.addEventListener('input', (event) => {
+    const value = formatstring(event.target.value);
+
+    const items = document.querySelectorAll('.items .item');
+    const noResults = document.getElementById('no_results');
+
+    let hasResults = false;
+
+    if (value != '') {
+        items.forEach((item) => {
+            const itemTitle = item.querySelector('.item-title').textContent;
+            const itemDescription = item.querySelector('.item-description').textContent;
+            if (formatstring(itemTitle).indexOf(value) !== -1 || formatstring(itemDescription).indexOf(value) !== -1) {
+                item.style.display = 'flex';
+
+                hasResults = true;
+            } else {
+                item.style.display = 'none';
+            }
+        });
+
+        if (hasResults) {
+            noResults.style.display = 'none';
+        } else {
+            noResults.style.display = 'block';
+        }
+    } else {
+        items.forEach(item => item.style.display = 'flex');
+
+        noResults.style.display = 'none';
+    }
+
+});
+
+function formatstring(value) {
+    return value
+        .toLowerCase()
+        .trim()
+}
